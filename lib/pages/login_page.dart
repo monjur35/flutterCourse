ca908bc92e0235c06d74f9878be9d72d3531518f
monjur35 <monjur35-1019@diu.edu.bd>
2022-01-11 18:31:39 +0600

    stateful widget

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/MyRoutes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name ="";
   @override
   Widget build(BuildContext context) {
+
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
           child: Column(children: [
             Image.asset("assets/images/login_image.png", fit: BoxFit.cover),
             const SizedBox(height: 20),
-            const Text(
-              "Welcome",
+            Text(
+              "Welcome $name",
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
               child: Column(children: [
                 TextFormField(
-                  decoration: const InputDecoration(
-                      hintText: "Enter username", labelText: "UserName"),
+                  decoration: const InputDecoration(hintText: "Enter username", labelText: "UserName"),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  }
                 ),
                 TextFormField(
                   obscureText: true,
@@ -35,10 +45,6 @@ class LoginPage extends StatelessWidget {
                 child: Text("Login"),
                 style: TextButton.styleFrom(minimumSize: Size(150, 40)),
                 onPressed: () {
-
-
-
-
                   Navigator.pushNamed(context, MyRoutes.homeRoute);
                 })
           ]),
