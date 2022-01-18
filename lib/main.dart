61a0b9e069c82f84fb040377239231140a9c06a7
monjur35 <monjur35-1019@diu.edu.bd>
2022-01-19 00:35:28 +0600

    models

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled/utils/MyRoutes.dart';
 import 'package:untitled/pages/home_page.dart';
 import 'package:untitled/pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:untitled/widgets/theme.dart';
 
 void main() {
