9324dfadba776a8cc7c8886b89c0573e6b146824
monjur35 <monjur35-1019@diu.edu.bd>
2022-01-25 22:43:14 +0600

    ClipRrect widget

--- lib/widgets/dayo5.dart
+++ lib/widgets/dayo5.dart
@@ -1,1 +1,20 @@
 import 'package:flutter/material.dart';
+
+class Day05 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        backgroundColor: Colors.grey,
+        body: Center(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              height: 300,
+              width: 300,
+              color: Colors.redAccent,
+            ),
+          ),
+        ));
+  }
+}
