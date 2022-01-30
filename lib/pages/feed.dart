6ab239a15cf31723d03626ffddd2966aa679fced
monjur35 <monjur35-1019@diu.edu.bd>
2022-01-30 18:00:02 +0600

    drawer

--- lib/pages/feed.dart
+++ lib/pages/feed.dart
@@ -1,6 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:untitled/pages/account.dart';
+import 'package:untitled/pages/home_page.dart';
 
 class Feed extends StatelessWidget {
+  final imageUrl =
+      "https://avatars.githubusercontent.com/u/70682148?s=400&u=6fdd99d9faca1dccf22c9f03b608dbffeecf3cd5&v=4";
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -14,18 +21,82 @@ class Feed extends StatelessWidget {
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
                 color: Colors.white)),
-        leading: IconButton(
-          onPressed: () {
-            //open Menu
-          },
-          icon: const Icon(Icons.menu),
-          color: Colors.white,
-        ),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.share))],
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.share,
+                color: Colors.white,
+              ))
+        ],
       ),
       body: const Center(
         child: Text(
           "Feed",
+        ),
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.red[300],
+          child: ListView(
+            children: [
+              DrawerHeader(
+                padding: EdgeInsets.zero,
+                child: UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(color: Colors.redAccent),
+                  margin: EdgeInsets.zero,
+                  accountName: const Text(
+                    "Monjur",
+                    textScaleFactor: 1.3,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  accountEmail: const Text(
+                    "monju@gmail.com",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(imageUrl),
+                  ),
+                ),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  ));
+                },
+                leading: Icon(Icons.home_outlined),
+                title: Text(
+                  'Home',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.feed),
+                title: Text(
+                  'Feed',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => Feed(),
+                  ));
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text(
+                  'Account',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => Account(),
+                  ));
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -4,32 +4,18 @@ import 'package:untitled/pages/account.dart';
 import 'package:untitled/pages/feed.dart';
 import 'package:untitled/pages/home_page.dart';
 import 'package:untitled/pages/messege.dart';
+import 'package:untitled/utils/MyRoutes.dart';
 
-class HomeDrawer extends StatefulWidget {
-  const HomeDrawer({Key? key}) : super(key: key);
-
-  _DrawerState createState() => _DrawerState();
-}
-
-class _DrawerState extends State<HomeDrawer> {
-  int _selectedIndex = 0;
-  final List<Widget> _pages = [HomePage(), Feed(), Messege(), Account()];
-
-  void _navigateBottomBar(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class HomeDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final imageUrl =
+    const imageUrl =
         "https://avatars.githubusercontent.com/u/70682148?s=400&u=6fdd99d9faca1dccf22c9f03b608dbffeecf3cd5&v=4";
     return Drawer(
         child: Container(
       color: Colors.indigo,
       child: ListView(
-        children: [
+        children: const [
           DrawerHeader(
             padding: EdgeInsets.zero,
             child: UserAccountsDrawerHeader(
